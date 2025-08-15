93e526117efa843fbe1c1aa7879f57cde9fee4b5
Bharadwaj Reddy <bharadwajreddy2004@gmail.com>
2025-08-15 16:33:32 +0530

    Updated calculator size in CSS

---
+++ script.js
@@ -0,0 +1,40 @@
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('.btn');
+
+let currentInput = '';
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const value = btn.innerText;
+    const action = btn.dataset.action;
+
+    if (action === 'AC') {
+      currentInput = '';
+      display.innerText = '0';
+    } 
+    else if (action === '=') {
+      try {
+        display.innerText = eval(currentInput);
+        currentInput = display.innerText;
+      } catch {
+        display.innerText = 'Error';
+      }
+    } 
+    else if (action === 'square') {
+      try {
+        currentInput = (eval(currentInput) ** 2).toString();
+        display.innerText = currentInput;
+      } catch {
+        display.innerText = 'Error';
+      }
+    } 
+    else if (action) {
+      currentInput += action;
+      display.innerText = currentInput;
+    } 
+    else {
+      currentInput += value;
+      display.innerText = currentInput;
+    }
+  });
+});
